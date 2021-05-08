28b768b0f112f3cbf22ef5d4946fed03f3aa7626
ajili <ilijastc062p@gmail.com>
2021-05-08 21:11:27 +0200

    css styling mostly on sidebar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import './App.css';
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import Channels from "./components/Channels";
 
 function App() {
   return (
     <div className="app">
       <Header />
-
       <div className="app__body">
         <Sidebar />
-        <Channels/>
       </div>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,8 @@ import db from '../firebase';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import CreateIcon from '@material-ui/icons/Create';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AddIcon from '@material-ui/icons/Add';
 import Channels from './Channels';
 
 function Sidebar() {
@@ -16,7 +18,7 @@ function Sidebar() {
     useEffect(() => {
         db.collection("channels").onSnapshot((snapshot) => { 
             setChannels(
-                snapshot.docs.map(doc => ({
+                snapshot.docs.map((doc) => ({
                     id: doc.id,
                     name: doc.data().name,
                 }))
@@ -37,8 +39,15 @@ function Sidebar() {
                 <MoreVertIcon/>
                 <h3>Browse Slack</h3>
             </div>
+            <div className="sidebar__showmore">
+                <div className="sidebar__showmore__channels">
+                    <ArrowDropDownIcon /> 
+                    <h3>Channels</h3>
+                </div>
+                <button><AddIcon/></button>
+            </div>
             {channels.map((channel) => {
-                <Channels title={channel.name} id={channel.id}/>
+                return <Channels title={channel.name} id={channel.id}/>
             })}
         </div>
     )
