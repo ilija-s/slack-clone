dd5807d4cafeed9f602055a551be3c0a11d83a29
ajili <ilijastc062p@gmail.com>
2021-05-21 23:05:26 +0200

    Added message component, rendered chat messages and channel info

--- src/components/Channels/Channels.js
+++ src/components/Channels/Channels.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
 import './Channels.css';
+import { useHistory } from 'react-router-dom';
 
 
-function Channels({title}) {
+function Channels({title, id}) {
+    const history = useHistory();
+
+    const openChannel = () => {
+        history.push(`/room/${id}`);
+    };
+
+    const openMenu = (e) => {
+    };
+
     return (
-        <div className="channels">
+        <div className="channels" onClick={openChannel} contextMenu={openMenu}>
             <h3><span className="channels__hash">#</span> {title}</h3>
         </div>
     )
