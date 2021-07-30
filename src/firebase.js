843e06fef7992ca2080f0df2ee907fd116821578
ajili <ilijastc062p@gmail.com>
2021-07-30 22:56:10 +0200

    firebase config change

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDnWp4ckNGEZEJx5LKOFF-0QZFw9CviUlw",
-    authDomain: "slack-clone-389c1.firebaseapp.com",
-    projectId: "slack-clone-389c1",
-    storageBucket: "slack-clone-389c1.appspot.com",
-    messagingSenderId: "883260424543",
-    appId: "1:883260424543:web:7d65f8a981a314569df6ea",
-    measurementId: "G-48MMW0RWD6"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
